8acefc994ce88bbc5dc791689ba73d491021f0c7
Maia Carolina Pozzolo <maia.pozzolo@auctane.com>
2022-05-07 12:52:02 +0200

    update

---
+++ lab sql intro_Maia.sql
@@ -0,0 +1,13 @@
+select * from sakila.actor;
+select * from sakila.film;
+select * from sakila.customer;
+
+select title from sakila.film;
+
+select distinct name from sakila.language;
+
+select count(store_id) from sakila.store;
+
+select count(staff_id) from sakila.staff;
+
+select first_name from sakila.staff;
